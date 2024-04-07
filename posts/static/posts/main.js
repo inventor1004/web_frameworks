4cd1a682274a69cc4e1496cf6bfad3f1072e657b
chris <117550785+inventor1004@users.noreply.github.com>
2024-04-07 17:29:50 -0400

    Getting Posts Objects with Ajax

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,6 +1,7 @@
 console.log('hello world');
 
 const helloWorldBox = document.getElementById('hello-world');
+const postsBox = document.getElementById('posts-box');
 
 
 $.ajax({
@@ -9,6 +10,25 @@ $.ajax({
   success: function(response){
     console.log('success', response.text)
     helloWorldBox.textContent = response.text
+  },
+  error: function(error){
+    console.log('error', error)
+  }
+})
+
+$.ajax({
+  type: 'GET',
+  url: '/data/5/',
+  success: function(response)
+  {
+    console.log(response)
+    const data = response.data
+    console.log(data)
+    data.forEach(el => {
+      postsBox.innerHTML += `
+        ${el.title} - <b>${el.body}</b><br>
+      `
+    })
   },
   error: function(error){
     console.log('error', error)
