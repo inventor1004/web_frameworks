0cd4a9abbe4260038e042a6a8fe364b78dfd5538
chris <117550785+inventor1004@users.noreply.github.com>
2024-04-07 18:23:05 -0400

    Creating Posts Cards

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -19,18 +19,32 @@ $.ajax({
 $.ajax({
   type: 'GET',
   url: '/data/5/',
-  success: function(response)
-  {
+  success: function(response){
     console.log(response)
     const data = response.data
     setTimeout(()=>{
-      spinnerBox.classList.add('not-visible')
+      // spinnerBox.classList.add('not-visible')
       console.log(data)
       data.forEach(el => {
       postsBox.innerHTML += `
-        ${el.title} - <b>${el.body}</b><br>
-      `
-    })
+        <div class="card mb-2">
+          <div class="card-body">
+            <h5 class="card-title">${el.title}</h5>
+            <p class="card-text">${el.body}</p>
+          </div>
+          <div class="card-footer">
+            <div class="row">
+              <div class="col-2">
+                <a href="#" class="btn btn-primary">Details</a>
+              </div>
+              <div class="col-2">
+                <a href="#" class="btn btn-primary">Like</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+    });
     }, 1000)
   },
   error: function(error){
